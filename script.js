9560eb460872047a06c9dfe09930324b108bf924
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-09-15 12:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let modifiedArr = [];
+let mp = {};
+for ( let i = 0; i < touristSpots.length; i++ ){
+    let str = touristSpots[i].replace(/\bThe\b/, "").trim();
+    modifiedArr.push(str);
+    mp[str] = touristSpots[i];
+}
+modifiedArr.sort();
+for ( let i = 0; i < modifiedArr.length; i++ ){
+    touristSpots[i] = mp[modifiedArr[i]];
+}
+console.log(touristSpots);
